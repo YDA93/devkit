06da2deb60de616e8da0ea528edc749a7d511eb9
Yousef Almutairi <yda93@hotmail.com>
2023-08-23 13:46:16 +0300

    Added django_delete_migrations

--- django/functions.sh
+++ django/functions.sh
@@ -52,6 +52,20 @@ django-translations-compile() {
 }
 
 
+# Delete migrations
+django_delete_migrations() {
+    # Get the current directory
+    project_directory="$PWD"
+
+    # Delete migration files in Django apps (excluding venv)
+    find "$project_directory" -path "*/migrations/*.py" -not -name "__init__.py" -not -path "$project_directory/venv/*" -exec sh -c 'echo "Deleted $(basename "$(dirname "$(dirname "{}")")")/$(basename "$(dirname "{}")")/$(basename "{}")"; rm "{}"' \;
+
+    # Delete migration cache in Django apps (excluding venv)
+    find "$project_directory" -type d -name "__pycache__" -not -path "$project_directory/venv/*" -exec sh -c 'echo "Deleted $(basename "$(dirname "{}")")/__pycache__"; rm -r "{}"' \;
+
+    echo "Deleted all Django migration files and __pycache__ folders (excluding venv)."
+}
+
 # Start Project
 django-start-project() {
     site_name=$1 # First Aurgment
