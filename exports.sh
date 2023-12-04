fcbe4712accfee659bb544f0522dd2fee58bfc04
Yousef Almutairi <yda93@hotmail.com>
2023-12-04 17:46:02 +0300

    More flutter features and added CCACHE_SLOPPINESS

--- exports.sh
+++ exports.sh
@@ -12,3 +12,8 @@ export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
 
 # Pub Path
 export PATH="$PATH":"$HOME/.pub-cache/bin"
+export CCACHE_SLOPPINESS=clang_index_store,file_stat_matches,include_file_ctime,include_file_mtime,ivfsoverlay,pch_defines,modules,system_headers,time_macros
+export CCACHE_FILECLONE=true
+export CCACHE_DEPEND=true
+export CCACHE_INODECACHE=true
+export CCACHE_LOGFILE=/Users/yousefalmutairi/Desktop/dev/ccache.log
