6f9f74dc817a82bd7fc403eea3aed3f09c951d74
Yousef Almutairi <yda93@hotmail.com>
2024-12-17 21:13:58 +0300

    Updated code

--- exports.sh
+++ exports.sh
@@ -10,8 +10,10 @@ export PATH="$PATH:/Users/yousefalmutairi/flutter/bin"
 # Ruby Path
 export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
 
-# Pub Path
-export PATH="$PATH":"$HOME/.pub-cache/bin"
+# Pub Path (for Dart & Flutter)
+export PATH="$PATH:$HOME/.pub-cache/bin"
+
+# CCACHE
 export CCACHE_SLOPPINESS=clang_index_store,file_stat_matches,include_file_ctime,include_file_mtime,ivfsoverlay,pch_defines,modules,system_headers,time_macros
 export CCACHE_FILECLONE=true
 export CCACHE_DEPEND=true
