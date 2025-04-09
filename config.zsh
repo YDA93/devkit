0e430b42431d239cd273850328a331f90d7bd4e7
Yousef Almutairi <yda93@hotmail.com>
2025-04-09 16:24:44 +0300

    Fixed install.zsh

--- config.zsh
+++ config.zsh
@@ -1,8 +1,5 @@
-# 🧩 Main entrypoint (works safely in Zsh)
+DEVKIT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]:-${0}}")" && pwd)"
 
-# Get the directory of this config.zsh file, reliably
-DEVKIT_CONFIG_DIR="${0:A:h}"
-
-export DEVKIT_ROOT="$DEVKIT_CONFIG_DIR"
+export DEVKIT_ROOT
 export DEVKIT_MODULES_DIR="$DEVKIT_ROOT/modules"
 export DEVKIT_ENTRYPOINT="$DEVKIT_ROOT/bin/devkit.zsh"
